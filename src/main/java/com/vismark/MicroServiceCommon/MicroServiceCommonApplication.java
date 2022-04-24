238ad54f085ffddc023a5c9f152e07000c74e87a
Vismark Juarez <vismarkjuarez@gmail.com>
2022-04-24 13:40:49 -0500

    Externalized the Person class into this project.

--- src/main/java/com/vismark/MicroServiceCommon/MicroServiceCommonApplication.java
+++ src/main/java/com/vismark/MicroServiceCommon/MicroServiceCommonApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroServiceCommonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceCommonApplication.class, args);
 	}
-
 }
